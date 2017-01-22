3d6b22179838f850d216bcc232fc658c927c1082
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2017-01-22 22:52:50 +0100

    initial version

--- src/App.js
+++ src/App.js
@@ -9,11 +9,62 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Catinder</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Patounez les chats que vous aimez, ou griffounez les méchants.
         </p>
+        <CatView cat="Malibu"/>
+      </div>
+    );
+  }
+}
+
+class CatView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.patoune = this.patoune.bind(this);
+    this.griffoune = this.griffoune.bind(this);
+    this.state = {
+      catName: "",
+      pictureUrl: ""
+    };
+  }
+  patoune() {
+    axios.put(`http://localhost:8080/cat/${this.props.cat}`, {
+      patounes: [ this.state.catName ]
+    }).then(req => {
+      this.displayNext()
+    });
+  }
+  griffoune() {
+    axios.put(`http://localhost:8080/cat/${this.props.cat}`, {
+      griffounes: [ this.state.catName ]
+    }).then(req => {
+      this.displayNext()
+    });
+  }
+  displayNext() {
+    axios.get(`http://localhost:8080/cat/${this.props.cat}/candidate/first`)
+      .then(res => {
+        this.setState({
+          catName: res.data.name,
+          pictureUrl: res.data.picture
+        });
+      });
+  }
+  componentDidMount() {
+    this.displayNext()
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.catName}</p>
+        <div>
+          <button onClick={this.patoune}>patoune</button>
+          <img src={this.state.pictureUrl} alt="{this.props.catName}" />
+          <button onClick={this.griffoune}>griffoune</button>
+        </div>
       </div>
     );
   }
